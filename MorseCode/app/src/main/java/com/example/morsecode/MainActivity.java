b1daadb47bc8e8e1ac56e5898ea9339bbc00081d
stefandr99 <stefandragoi99@gmail.com>
2021-05-12 19:28:22 +0300

    added main layout

--- MorseCode/app/src/main/java/com/example/morsecode/MainActivity.java
+++ MorseCode/app/src/main/java/com/example/morsecode/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.morsecode;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button startMorse;
+    Button infoMorse;
+    Button exitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startMorse = (Button) findViewById(R.id.startMorseButton);
+        infoMorse = (Button) findViewById(R.id.infoMorseButton);
+        exitButton = (Button) findViewById(R.id.exitButton);
+
+        startMorse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainMorseActivity();
+            }
+        });
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exitApplication();
+            }
+        });
+    }
+
+    public void openMainMorseActivity() {
+        Intent intent = new Intent(this, MainMorseActivity.class);
+        startActivity(intent);
+    }
+
+    public void exitApplication() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage("Sunteți sigur că doriți să ieșiți?")
+                .setCancelable(false)
+                .setNegativeButton("Nu", (dialog, which) -> dialog.cancel())
+                .setPositiveButton("Da", (dialog, which) -> finish());
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
